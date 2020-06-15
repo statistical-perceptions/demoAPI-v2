a36837e453f1ec6cb9f6532a5c7a2bc31662c4b7
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-15 17:12:57 -0400

    PUT is working!

--- server.js
+++ server.js
@@ -46,9 +46,38 @@ app.post('/api/feedback', (req, res) => {
         q1: req.body.q1
     }
     // this is for demo / dev
+    // only updating the array of demoData, changes are not permanent 
+    // because there are no online database
     demoData.push(entry);
     // res with entry object itself
     res.json(entry);
+});
+
+// PUT
+app.put('/api/feedback/:userID', (req, res) => {
+    const requestID = req.params.userID;
+    // filter down:
+    // demoEntry is an array with 1 element
+    let demoEntry = demoData.filter(demoEntry => {
+        return demoEntry.userID == requestID;
+    })[0];
+    // storing the index of the incoming entry change
+    const index = demoData.indexOf(demoEntry)
+    // all the keys from req.body
+    const keys = Object.keys(req.body);
+    // loop through each kv pair to do necessary updates
+    keys.forEach(key => {
+        // update each value according to key
+        demoEntry[key] = req.body[key];
+    })
+    // update the demoData 
+    demoData[index] = demoEntry
+    // handle error
+    if (!demoEntry) {
+        res.status(404).json({message : 'No demoEntry found'});
+    }
+    // return the modified entry
+    res.json(demoData[index]);
 });
 
 app.listen(port, hostname, () => {
