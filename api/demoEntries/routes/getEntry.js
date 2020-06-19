04d175a79c230feba76af5261a85ad46580c462a
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-19 11:06:38 -0400

    changed col from var to const

--- api/demoEntries/routes/getEntry.js
+++ api/demoEntries/routes/getEntry.js
@@ -10,14 +10,14 @@ const router = express.Router();
 // '/' is based on /api/feedback
 router.route('/:col')
     .get((req, res) => {
-        var col = req.params.col;
+        const col = req.params.col;
         const generalSchema = new Schema({
             userID: { type: String, required: false},
             sliderVal: { type: String, required: false},
             q1: { type: String, required: false},
             q2: { type: String, required: false},
             q3: { type: String, required: false}},
-            { collection : col.toString });
+            { collection : col });
         // Collection.changeTo(col);
         // var Entry = require('../model/demoEntry');
         // provide an object with find, you can specify what we want to find
