4e4b2b769b2e0dc1bd5c75240108d3d3b9be96f8
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-19 18:12:21 -0400

    get should still be working

--- api/demoEntries/routes/getEntry.js
+++ api/demoEntries/routes/getEntry.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 // const Collection = require('../model/changeCol');
-var Entry = require('../model/demoEntry');
+// var Entry = require('../model/demoEntry');
 const Schema = mongoose.Schema;
 // use router to manage relative paths
 const router = express.Router();
@@ -15,8 +15,15 @@ router.route('/')
         // var theKeys = Object.keys(reqBody);
         // var col_name = reqBody[theKeys[0]];
         // Collection.changeTo(col);
+        const demoEntrySchema = new Schema({
+            userID: { type: String, required: true},
+            sliderVal: { type: String, required: true},
+            q1: { type: String, required: false},
+            q2: { type: String, required: false},
+            q3: { type: String, required: false}},
+            { collection : "entries"});
         // provide an object with find, you can specify what we want to find
-        Entry.find({}, (err, entries) => {
+        demoEntrySchema.find({}, (err, entries) => {
             if (err) {
                 res.status(400).json(err);
             };
