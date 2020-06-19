c3cb6ac1072bb828626f3eb55dc0bba8ad35a18a
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-19 09:26:25 -0400

    Updated getEntry

--- api/demoEntries/routes/getEntry.js
+++ api/demoEntries/routes/getEntry.js
@@ -9,11 +9,11 @@ const router = express.Router();
 // '/' is based on /api/feedback
 router.route('/')
     .get((req, res) => {
-        const col = req.params.col;
+        const col = req.params["col"];
         Collection.changeTo(col);
         const Entry = require('../model/demoEntry');
         // provide an object with find, you can specify what we want to find
-        Entry.find({ col }, (err, entries) => {
+        Entry.find({ }, (err, entries) => {
             if (err) {
                 res.status(400).json(err);
             };
