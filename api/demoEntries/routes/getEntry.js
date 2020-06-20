74de95508d84d487364b0fd2d221f9de4a3f08ff
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-19 20:09:13 -0400

    Changed to single quotes around collection name

--- api/demoEntries/routes/getEntry.js
+++ api/demoEntries/routes/getEntry.js
@@ -15,13 +15,14 @@ router.route('/')
         // var theKeys = Object.keys(reqBody);
         // var col_name = reqBody[theKeys[0]];
         // Collection.changeTo(col);
-        var demoEntrySchema = new mongoose.Schema({
+        var Schema = mongoose.Schema;
+        var demoEntrySchema = new Schema({
             userID: { type: String, required: true},
             sliderVal: { type: String, required: true},
             q1: { type: String, required: false},
             q2: { type: String, required: false},
             q3: { type: String, required: false}},
-            { collection : "entries"});
+            { collection : 'entries' });
         var Entry = mongoose.model('Entry', demoEntrySchema);
         // provide an object with find, you can specify what we want to find
         Entry.find({}, (err, entries) => {
