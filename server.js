12426c2c9d00013d0fc1eb347ce5e67f87df0ce9
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-22 12:09:01 -0400

    Changed to production mode

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 const hostname = 'localhost';
 const dev_port = 3000;
 
-var ENV = 'dev';
+var ENV = 'prod';
 
 mongoose.connect(mongooseURI, dbOptions, (err) => {
     if (err) {
