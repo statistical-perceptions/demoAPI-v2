55c7387f8773c5cfd4bb5b47a98245cdde3aab78
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-23 18:07:47 -0400

    Added username and password fields in schema

--- api/demoEntries/model/demoEntry.js
+++ api/demoEntries/model/demoEntry.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const demoEntrySchema = new Schema({
-    userID: { type: String, required: true},
-    sliderVal: { type: String, required: true},
+    userID: { type: String, required: false},
+    sliderVal: { type: String, required: false},
     q1: { type: String, required: false},
     q2: { type: String, required: false},
-    q3: { type: String, required: false}});
+    q3: { type: String, required: false},
+    username: { type: String, required: false},
+    password: { type: String, required: false}});
 
 // use Entry to reference our model in other files.
 module.exports = mongoose.model('Entry', demoEntrySchema);
