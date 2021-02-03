65400ceccbc5c706f9132c6c3b28d3735a39c6d1
elVictor <39276857+Elvictor01@users.noreply.github.com>
2021-02-03 10:41:07 +0100

    Update liveTest.js
    Added comments to make the code more readable

--- test/liveTest.js
+++ test/liveTest.js
@@ -1,16 +1,17 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require("chai"); //Assertion library import
+const chaiHttp = require("chai-http"); // for http integration testing
 const expect = chai.expect
-const baseUrl = "https://test-api-615.herokuapp.com"
+const baseUrl = "https://test-api-615.herokuapp.com" //base url where the routes are rooted
 
 chai.use(chaiHttp);
 describe("Server Test", function(){
+//Test to check if server is live
 it('server is live', function(done) {
         chai.request(baseUrl)
         .get('/')
         .end(function (err, res) {
             expect(res).to.have.status(200);
-            expect(res.text).to.equal("Use /api/feedback/collection_name/some_userID");
+
             done();
         });
     })
